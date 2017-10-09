2e9f77af0f6e75759aa99404ba274994b2363465
baranbartu <bbartu.demirci@gmail.com>
2017-10-09 21:34:59 +0300

    used lambda functions instead of evaulating code on runtime

--- rulengine/__init__.py
+++ rulengine/__init__.py
@@ -1,4 +1,3 @@
-from rulengine.core import stdoutIO, CodeGenerator
 from rulengine.conditions import make_condition
 
 VERSION = (0, 0, 3)
@@ -6,29 +5,23 @@ __version__ = '.'.join(map(str, VERSION))
 
 
 def execute(rules):
-    code = generate_executable_code(rules)
-    with stdoutIO() as s:
-        exec code
-    return bool(int(s.getvalue()))
+    conditions = generate_conditions(rules)
+    return execute_conditions(conditions)
 
 
-def generate_executable_code(rules):
-    c = CodeGenerator()
-    c.initialize('    ')
-    if_statement = 'if '
-    for index, rule in enumerate(rules):
+def generate_conditions(rules):
+    conditions = []
+    for rule in rules:
         cond = make_condition(rule)
-        if_statement += cond
-        if index < len(rules) - 1:
-            if_statement += ' and '
-    if_statement += ':'
-    c.write(if_statement)
-    c.indent()
-    c.write('print 1')
-    c.dedent()
-    c.write('else:')
-    c.indent()
-    c.write('print 0')
-    c.dedent()
-    code = c.end()
-    return code
+        conditions.append(cond)
+    return conditions
+
+
+def execute_conditions(conditions):
+    result = False
+    for cond in conditions:
+        # each cond is a lambda function that was prepared in conditions
+        result = cond()
+        if result:
+            break
+    return result
--- rulengine/core.py
+++ rulengine/core.py
@@ -1,12 +1,10 @@
 import six
 import sys
-import string
-import StringIO
-import contextlib
 from collections import namedtuple
 
 Rule = namedtuple('Rule', ('value', 'operator',
                            'comparison_value', 'data_structure'))
+Condition = namedtuple('Condition', ('value', 'func', 'comparison_value'))
 
 
 class Operator:
@@ -17,15 +15,17 @@ class Operator:
     LESS_EQUAL = 'less_equal'
     GREATER_EQUAL = 'greater_equal'
     IN = 'in'
+    IN_COMMA = 'in_comma'
 
-    MAPPING = {
-        EQUAL: '==',
-        NOT_EQUAL: '!=',
-        LESS: '<',
-        GREATER: '>',
-        LESS_EQUAL: '<=',
-        GREATER_EQUAL: '>=',
-        IN: 'in'
+    FUNC_MAPPING = {
+        EQUAL: lambda x, y: x == y,
+        NOT_EQUAL: lambda x, y: x != y,
+        LESS: lambda x, y: x < y,
+        GREATER: lambda x, y: x > y,
+        LESS_EQUAL: lambda x, y: x <= y,
+        GREATER_EQUAL: lambda x, y: x >= y,
+        IN: lambda x, y: x in y,
+        IN_COMMA: lambda x, y: x in y
     }
 
 
@@ -33,38 +33,7 @@ class DataStructure:
     STRING = 'str'
     INTEGER = 'int'
     FLOAT = 'float'
-
-
-class CodeGenerator(object):
-
-    def initialize(self, tab='\t'):
-        self.code = []
-        self.tab = tab
-        self.level = 0
-
-    def end(self):
-        return string.join(self.code, '')
-
-    def write(self, string):
-        self.code.append(self.tab * self.level + string + '\n')
-
-    def indent(self):
-        self.level = self.level + 1
-
-    def dedent(self):
-        if self.level == 0:
-            raise SyntaxError('Unknown error in code generator')
-        self.level = self.level - 1
-
-
-@contextlib.contextmanager
-def stdoutIO(stdout=None):
-    old = sys.stdout
-    if stdout is None:
-        stdout = StringIO.StringIO()
-    sys.stdout = stdout
-    yield stdout
-    sys.stdout = old
+    DATE = 'date'
 
 
 if six.PY3:
@@ -75,7 +44,7 @@ else:
         return sys.modules[name]
 
 
-def _import_class(class_path):
+def import_class(class_path):
     module_name, class_name = class_path.rsplit('.', 1)
     module = _import_module(module_name)
     return getattr(module, class_name)
